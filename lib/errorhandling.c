611210275eab152a4aec4ac4d0d54d9229419ed7
yonshin <yonshin@student.42seoul.kr>
2022-11-01 22:22:54 +0900

    norm

--- lib/errorhandling.c
+++ lib/errorhandling.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errorhandling.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonshin <yonshin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/01 22:18:20 by yonshin           #+#    #+#             */
+/*   Updated: 2022/11/01 22:22:34 by yonshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include "libft.h"
 
-void    errorhandling(char *msg)
+void	errorhandling(char *msg)
 {
-    if (DEBUG_MODE == 1)
-        ft_putstr_fd(msg, 2);
-    else
-        ft_putstr_fd("Error\n", 2);
-    exit(1);
+	if (DEBUG_MODE == 1)
+		ft_putstr_fd(msg, 2);
+	else
+		ft_putstr_fd("Error\n", 2);
+	exit(1);
 }
